ab91f13e8492b496da51f5966bff4ce458705b70
Felipe Reis <felipeareis@protonmail.com>
2021-09-18 23:54:30 +0100

    created register view. created input, button and transaction type button components

---
+++ src/pages/Register/index.tsx
@@ -0,0 +1,62 @@
+import React, { useState } from 'react';
+
+import { Input } from '../../components/Form/Input';
+import { Button } from '../../components/Form/Button';
+import { TransactionTypeButton } from '../../components/Form/TransactionTypeButton';
+
+import {
+  Container,
+  Header,
+  Title,
+  Form,
+  Fields,
+  TransactionTypeButtonsContainer
+} from './styles';
+
+export function Register() {
+  const [transactionType, setTransactionType] = useState('');
+
+  function handleTransactionTypeSelect(type: 'up' | 'down') {
+    setTransactionType(type);
+  }
+
+  return (
+    <Container>
+      <Header>
+        <Title>Cadastro</Title>
+      </Header>
+
+      <Form>
+        <Fields>
+          <Input
+            placeholder="Nome"
+          />
+
+          <Input
+            placeholder="Preço"
+          />
+
+          <TransactionTypeButtonsContainer>
+            <TransactionTypeButton
+              title="Income"
+              type="up"
+              onPress={() => handleTransactionTypeSelect('up')}
+              isActive={transactionType === 'up'}
+            />
+            <TransactionTypeButton
+              title="Outcome"
+              type="down"
+              onPress={() => handleTransactionTypeSelect('down')}
+              isActive={transactionType === 'down'}
+            />
+          </TransactionTypeButtonsContainer>
+        </Fields>
+
+        <Button
+          title="Enviar"
+        />
+      </Form>
+
+    </Container>
+  );
+}
